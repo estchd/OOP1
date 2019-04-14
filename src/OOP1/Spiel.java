5a671f421a8d7bbd0af3d5bcaecff58cf046fc3e
amyjosi <49399582+amyjosi@users.noreply.github.com>
2019-04-14 13:11:55 +0200

    Spiel und Steinhaufen Teil 1

--- src/OOP1/Spiel.java
+++ src/OOP1/Spiel.java
@@ -2,7 +2,13 @@ package OOP1;
 
 import java.util.List;
 
-@SuppressWarnings("unused")
+/** @author Josephine
+ * "Nimm-Spiel"
+ * Zwei Spieler nehmen Steine von einem Steinhaufen bis kein Stein mehr vorhanden ist.
+ * Die Anzahl der Steine im Steinhaufen ist zufällig zwischen 20 und 30, sowie ist auch
+ * der Startspieler zufällig. Die Spieler nehmen im Wechsel 1 bis 3 Steine vom Steinhaufen.
+ * Verloren hat der Spieler, der den letzten Stein nehmen muss.
+ */
 class Spiel {
 
     private int spieleranzahl;
@@ -11,16 +17,30 @@ class Spiel {
 
     @SuppressWarnings("unused")
     public Spiel(int spieleranzahl){
-
+        // er braucht spieler - mit namen, und nicht den int
     }
 
+    /**
+     * Spielernamen festlegen?
+     * Spielregeln
+     * wie viele steine im steinhaufen gesamt
+     * startspieler
+     * Rundenschleife - spieler nimmt wie viele Steine, wie viele sind übrig
+     */
     @SuppressWarnings("EmptyMethod")
     public void spielStart(){
+        //Hier sind die Grenzen des Steinhaufens festgelegt, somit brauche ich hier keine Exception, falls max >= min
+        steinhaufen = new Steinhaufen(20,30);
 
     }
 
     @SuppressWarnings({"SameReturnValue", "unused"})
     public String getSpielregeln(){
-        return "";
+        String regeln = " Spielregeln des Nim-Spiels: \n" +
+                " Zwei Spieler nehmen Steine von einem Steinhaufen bis kein Stein mehr vorhanden ist. \n" +
+                " Die Anzahl der Steine im Steinhaufen ist zufällig zwischen 20 und 30, sowie ist auch \n" +
+                " der Startspieler zufällig. Die Spieler nehmen im Wechsel 1 bis 3 Steine vom Steinhaufen.\n" +
+                " Verloren hat der Spieler, der den letzten Stein nehmen muss.";
+        return regeln;
     }
 }
--- src/OOP1/Steinhaufen.java
+++ src/OOP1/Steinhaufen.java
@@ -1,21 +1,42 @@
 package OOP1;
 
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * @author Josephine
+ * Die Steinhaufen-Klasse gibt die Anzahl der Steine im Steinhaufen aus,
+ * sowie bietet sie eine Methode an, 1-3 Steine aus dem Steinhaufen zu entnehmen
+ */
 @SuppressWarnings("unused")
 class Steinhaufen {
 
-    private int anzahlSteine;
+    private int anzSteine;
 
     public Steinhaufen(int minAnzSteine, int maxAnzSteine){
-
+        this.anzSteine =  ThreadLocalRandom.current().nextInt(minAnzSteine, maxAnzSteine + 1);
     }
 
     @SuppressWarnings("SameReturnValue")
-    public int getAnzahlSteine(){
-        return 0;
+    public int getAnzSteine(){
+        return anzSteine;
     }
 
     @SuppressWarnings("EmptyMethod")
-    public void nimmSteine(int anzahlSteine){
+    /**
+     * Exception in Main behandelt, Exception " bitte anderen Wert eingeben, weil..."
+     * wdh der Eingabe
+     */
+    public void nimmSteine(int anzahlSteine) throws Exception{
+        if (anzahlSteine >= 1 && anzahlSteine<=3) {
+            if (anzSteine >= anzahlSteine) {
+                this.anzSteine = this.anzSteine - anzahlSteine;
+                System.out.println("Es wurden " + anzahlSteine + " aus dem Steinhaufen genommen.");
+            } else {
+                throw Exception;
+            }
+        } else {
+            throw Exception;
+        }
 
     }
 }
